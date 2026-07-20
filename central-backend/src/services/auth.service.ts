9df0dd4fc02172e6806d5031f52f2871a260b307
dilusewhpd <dilunisewwandi10@gmail.com>
2026-07-20 12:10:02 +0530

    refactor: convert AuthService to a standalone register function

--- central-backend/src/services/auth.service.ts
+++ central-backend/src/services/auth.service.ts
@@ -1,47 +1,54 @@
 import bcrypt from "bcrypt";
-import { RegisterInput } from "../modules/auth/auth.validation.js";
 import { prisma } from "../config/prismaDb.js";
+import { RegisterInput } from "../modules/auth/auth.validation.js";
 import { ROLES } from "../utils/constants/auth.constants.js";
 
-export class AuthService {
-  async register(data: RegisterInput) {
-    const existingUser = await prisma.user.findUnique({
-      where: {
-        email: data.email,
-      },
-    });
+export const register = async (data: RegisterInput) => {
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
 
-    if (existingUser) {
-      throw new Error("Email is already registered.");
-    }
-
-    const hashedPassword = await bcrypt.hash(data.password, 10);
-
-    const role = await prisma.role.findUnique({
-      where: {
-        name: ROLES.FIELD_OFFICER, // Assigning the default role as FIELD_OFFICER
-      },
-    });
-
-    if (!role) {
-      throw new Error("Default role not found.");
-    }
-
-    const user = await prisma.user.create({
-      data: {
-        firstName: data.firstName,
-        lastName: data.lastName,
-        email: data.email,
-        passwordHash: hashedPassword,
-        roleId: role.id,
-      },
-      include: {
-        role: true,
-      },
-    });
-
-    const { passwordHash, ...safeUser } = user;
-
-	return safeUser;
+  if (existingUser) {
+    throw new Error("Email is already registered.");
   }
-}
+
+  const hashedPassword = await bcrypt.hash(data.password, 10);
+
+  const role = await prisma.role.findUnique({
+    where: {
+      name: ROLES.FIELD_OFFICER,
+    },
+  });
+
+  if (!role) {
+    throw new Error("Default role not found.");
+  }
+
+  const user = await prisma.user.create({
+    data: {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.email,
+      passwordHash: hashedPassword,
+      roleId: role.id,
+    },
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+      email: true,
+      isActive: true,
+      createdAt: true,
+      role: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+    },
+  });
+
+  return user;
+};

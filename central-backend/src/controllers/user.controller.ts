2af5394da3244a477d0d0bfa4a65d698c5d72eb1
dilusewhpd <dilunisewwandi10@gmail.com>
2026-07-22 18:03:03 +0530

    feat: add getUsers route with query validation for retrieving users

--- central-backend/src/controllers/user.controller.ts
+++ central-backend/src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
 import * as userService from "../services/user.service.js";
+import { GetUsersQuery } from "../validators/user.validation.js";
 
 export const createUser = async (
   req: Request,
@@ -23,4 +24,24 @@ export const createUser = async (
     next(error);
   }
 
+};
+
+export const getUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const query = req.validatedQuery as GetUsersQuery;
+
+	const result = await userService.getUsers(query);
+	
+    res.status(200).json({
+      success: true,
+      message: "Users retrieved successfully.",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
--- central-backend/src/routes/user.routes.ts
+++ central-backend/src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createUser } from "../controllers/user.controller.js";
+import { createUser, getUsers } from "../controllers/user.controller.js";
 import { authenticate } from "../middlewares/authenticate.js";
 import { authorize } from "../middlewares/authorize.js";
 import { validate } from "../middlewares/validate.js";
 import { ROLES } from "../utils/constants/auth.constants.js";
-import { createUserSchema } from "../validators/user.validation.js";
+import { createUserSchema, getUsersQuerySchema } from "../validators/user.validation.js";
+import { validateQuery } from "../middlewares/validateQuery.js";
 
 const router: ReturnType<typeof Router> = Router();
 
@@ -14,6 +15,14 @@ router.post(
   authorize(ROLES.ADMIN),
   validate(createUserSchema),
   createUser
+);
+
+router.get(
+  "/",
+  authenticate,
+  authorize(ROLES.ADMIN),
+  validateQuery(getUsersQuerySchema),
+  getUsers
 );
 
 export default router;
